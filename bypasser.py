b7ca4ea234265ab682515a6cb82b533bc6275523
BotCreator99 <botcreator99@onmail.com>
2022-12-24 19:23:39 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -10,9 +10,9 @@ import threading
 from texts import HELP_TEXT, ABOUT_TEXT
 # Follow on GitHub @BotCreator99
 # Update key and token 
-bot_token = os.environ.get("TOKEN", "5731935556:AAE6gkDi86bCF9fn7kn-wRaEe8RnZ6u_R0w")
-api_hash = os.environ.get("HASH", "1168e573def0c74a7e6e68dae9313c68")
-api_id = os.environ.get("ID", "11983148")
+bot_token = os.environ.get("TOKEN", "")
+api_hash = os.environ.get("HASH", "")
+api_id = os.environ.get("ID", "")
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 
