4fb051e36d160925d9dc389333c3d1d262f5c66d
GreyMatter's <greymattersyt@gmail.com>
2022-12-28 09:46:20 +0530

    Update texts.py

--- texts.py
+++ texts.py
@@ -2,6 +2,6 @@ HELP_TEXT = """👊Cᴏɴᴛᴀᴄᴛ ʀᴇᴘᴏʀᴛ ʙᴜɢ :-@BotCreator99""
 
 ABOUT_TEXT = """
 <b>⚜ Mʏ ɴᴀᴍᴇ :- ShareusBypasserBot</b>\n
-<b>🔸Vᴇʀꜱɪᴏɴ :- <a href='https://telegram.me/BotMinister'>1.0</a></b>\n
+<b>🔸Subscribe :- <a href='https://youtube.com/@GreyMattersYT'>1.0</a></b>\n
 <b>🔹Dᴇᴠᴇʟᴏᴘᴇʀ :- <a href='https://t.me/BotCreator99'>BotCreator99</a></b>\n
-<b>🔸Lᴀꜱᴛ ᴜᴘᴅᴀᴛᴇᴅ :- <a href='https://telegram.me/BotMinister'>[ 19-12-2022 ]</a></b>"""
+<b>🔸Lᴀꜱᴛ ᴜᴘᴅᴀᴛᴇᴅ :- <a href='https://telegram.me/GreyMatter_Bots'>[ 19-12-2022 ]</a></b>"""
